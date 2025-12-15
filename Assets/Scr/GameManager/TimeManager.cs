e4bac2833b90560afe684f1b0be2abea79ccbded
kino1022 <yuta_fujita@outlook.com>
2025-12-15 21:47:08 +0900

    update

--- Assets/Scr/GameManager/TimeManager.cs
+++ Assets/Scr/GameManager/TimeManager.cs
@@ -1,14 +1,6 @@
+using System.Threading;
 using Cysharp.Threading.Tasks;
 
 namespace Scr.GameManager {
-    public static class TimeManager {
 
-        private static float _currentTime = 0.0f;
-        
-        public static float Time => _currentTime;
-
-        private static async UniTask CountTime() {
-            
-        }
-    }
 }
