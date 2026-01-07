b04970cd4774a0049357f2b0ae11899f05b6385a
kino1022 <132138562+kino1022@users.noreply.github.com>
2026-01-07 12:54:45 +0900

    update

--- Assets/Scr/Stage/WarpPipeBehaviourController.cs
+++ Assets/Scr/Stage/WarpPipeBehaviourController.cs
@@ -8,6 +8,7 @@ using Scr.Utility;
 using UnityEngine;
 using Sirenix.Serialization;
 using Sirenix.OdinInspector;
+using Unity.Cinemachine;
 
 namespace Assets.Scr.Stage {
     public class WarpPipeBehaviourController : MonoBehaviour {
@@ -17,6 +18,12 @@ namespace Assets.Scr.Stage {
 
         [SerializeField]
         private Transform _standPoint;
+
+        [SerializeField]
+        private WarpPipeBehaviourController _warpPoint;
+
+        [SerializeField]
+        private Collider2D _nextCameraArea;
 
         [SerializeField]
         private IInputModule<Vector2> _moveModule;
@@ -40,13 +47,19 @@ namespace Assets.Scr.Stage {
         [SerializeField]
         [ReadOnly]
         private Player _cachedPlayerObj = null;
+        
+        [SerializeField]
+        [ReadOnly]
+        private CinemachineConfiner2D _cameraConfiner = null;
 
+        private Collider[] _overlappedColliders = new Collider[] { };
         private Vector3 _cachedCenterDireciton = Vector3.zero;
         private bool _registeredInput = false;
 
 
         private void Start() {
             _cachedCenterDireciton = _pipeCenter.position - _standPoint.position;
+            _cameraConfiner = FindAnyObjectByType<CinemachineConfiner2D>();
             RegisterWarpInput();
         }
 
@@ -68,7 +81,7 @@ namespace Assets.Scr.Stage {
                     GetAngle(signal.Value) <= GetAngle(_cachedCenterDireciton) - _inputThreshold;
             }
 
-            if (_moveModule is not null) {
+            if (_moveModule is null) {
                 return;
             }
             _moveModule
@@ -82,18 +95,14 @@ namespace Assets.Scr.Stage {
         }
 
         private void GetPlayerStand () {
-            var hits = Physics.OverlapSphere(_standPoint.position, _collisionRadius);
-            for (int i = 0; i < hits.Length; i++) {
-                var hit = hits[i];
+            Physics.OverlapSphereNonAlloc(_pipeCenter.position, _collisionRadius, _overlappedColliders);
+            for (int i = 0; i < _overlappedColliders.Length; i++) {
+                var hit = _overlappedColliders[i];
                 var player = hit.gameObject.GetComponentFromWhole<Player>();
                 if (player is not null) {
                     _onPlayerStanding = true;
                     _cachedPlayerObj = player;
                     return;
-                }
-                else {
-                    _onPlayerStanding = false;
-                    _cachedPlayerObj = null;
                 }
             }
             _onPlayerStanding = false;
@@ -109,12 +118,56 @@ namespace Assets.Scr.Stage {
                 rigidBody.isKinematic = true;
             }
 
-            await MoveTransitionAsync();
+            await MoveTransitionAsync(
+                _cachedPlayerObj.transform,
+                _standPoint.position,
+                _pipeCenter.position,
+                _animationDuration
+                );
+
+            _cameraConfiner ??= FindAnyObjectByType<CinemachineConfiner2D>();
+            _cameraConfiner.BoundingShape2D = _nextCameraArea;
+            await _warpPoint.ExitSequenceAsync();
 
         }
+        
+        private async UniTask ExitSequenceAsync () {
+            if (_cachedPlayerObj is null || _warpPoint is null) {
+                return;
+            }
+            var rigidBody = _cachedPlayerObj.gameObject.GetComponentFromWhole<Rigidbody>();
+            if (rigidBody is not null) {
+                rigidBody.isKinematic = true;
+            }
 
-        private async UniTask MoveTransitionAsync () {
+            await MoveTransitionAsync(
+                _cachedPlayerObj.transform,
+                _warpPoint._pipeCenter.position,
+                _warpPoint._standPoint.position,
+                _animationDuration
+                );
 
+            if (rigidBody is not null) {
+                rigidBody.isKinematic = false;
+            }
+        }
+
+        private async UniTask MoveTransitionAsync (Transform target, Vector3 from, Vector3 to, float duration) {
+            //最初に強制的に移動させる
+            target.position = from;
+            float time = 0;
+            while (time < duration) {
+                time += Time.deltaTime;
+                float t = time / duration;
+                // 簡易イージング (SmoothStep)
+                t = t * t * (3f - 2f * t); 
+                target.position = Vector3.Lerp(from, to, t);
+                await UniTask.Yield(
+                    PlayerLoopTiming.Update,
+                    this.GetCancellationTokenOnDestroy()
+                    );
+            }
+            target.position = to;
         }
     }
 }
